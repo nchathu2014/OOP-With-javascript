6eaa3575207e895533c79df0431628073a0501e3
Chathuranga <UCHATNU@PEROOT.COM>
2016-07-17 19:39:28 +0530

    add files

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@
 
 class Drone{
 
+    constructor(id,name){
+        this.id=id;
+        this.name=name;
+    }
 }
-let droneInstance = new Drone();
+let droneInstance = new Drone("A123","Black Bird");
+let droneInstance1 = new Drone("A1233","Black Hawek");
 
-console.log("Type of Drone : "+typeof Drone);
-console.log("Instance of Drone : "+typeof droneInstance);
+console.log(droneInstance["id"]+"--"+droneInstance["name"]);
+console.log(droneInstance1.id+"--"+droneInstance1.name);
